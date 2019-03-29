e80d5a85389136cd5c5f36e2041b4bab08c8e3dd
xiaochen hu <jason95328@hotmail.com>
2019-03-29 16:08:23 -0400

    Admin
    change the text

--- NGO/NGO/urls.py
+++ NGO/NGO/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "NGO Admin"
+admin.site.site_title = "NGO Admin Portal"
+admin.site.index_title = "Welcome to NGO"
