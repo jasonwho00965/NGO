396f79c6def9bf4ca3e011d99ecfe5bd2b8ce669
xiaochen hu <jason95328@hotmail.com>
2019-04-01 13:42:06 -0400

    Admin page
    April 1 2019 1:42 pm

--- NGO/app/admin.py
+++ NGO/app/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import User
+from .models import UserManagement
+from .models import EventManagement
+from django.contrib.auth.models import User, Group
+from django.utils.html import format_html
 
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 # Gives us access to this data within the Admin page. At least I think so - Ed.
-@admin.register(User)
+
 class AdminDisplay(admin.ModelAdmin):
-	list_display = ['first_name', 'last_name', 'email', 'role']
+
+	list_display = ['first_name', 'last_name','email','role']
+
+
+class EventDisplay(admin.ModelAdmin):
+	list_display = ['Event','Category','Location','Start_date','End_date']
+
+
+
+admin.site.register(EventManagement,EventDisplay)
+admin.site.register(UserManagement, AdminDisplay)
+
--- NGO/app/models.py
+++ NGO/app/models.py
@@ -2,37 +2,44 @@ from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
-    ROLE_CHOICES = [('A', 'Admin'), ('U', 'User')]
+class UserManagement(models.Model):
+    ROLE_CHOICES = [('U','User'),('A','Admin')]
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
-    email = models.EmailField()
+    email = models.CharField(max_length=25)
     password = models.CharField(max_length=20)
     """Testing blank=True on role. If any problems arise, let Ed know."""
-    role = models.CharField(choices=ROLE_CHOICES, max_length=1, blank=True)
+    role = models.CharField(choices=ROLE_CHOICES, max_length=5, blank=True)
 
     """Enables admin to view data other than "User object" within admin screen."""
-    def __str__(self):
-        return self.first_name, self.last_name, self.email, self.role
+    #def __str__(self):
+        #return self.first_name, self.last_name, self.email, self.password, self.role
 
 
-class Event(models.Model):
+class EventManagement(models.Model):
     EVENT_CHOICES = [('Con', 'Conference'),
                      ('Sem', 'Seminar'),
-                     ('Pres', 'Presentation'),
+                     ('Pres', 'Presentation')
                      ]
-    e_name = models.CharField(max_length=35)
+    Event = models.CharField(max_length=35)
     e_description = models.TextField(max_length=255)
-    e_category = models.CharField(choices=EVENT_CHOICES, max_length=4)
-    e_start_date = models.DateField()
-    e_end_date = models.DateField()
+    Category = models.CharField(choices=EVENT_CHOICES, max_length=4)
+    Start_date = models.DateField()
+    End_date = models.DateField()
     e_start_time = models.DateTimeField()
     e_end_time = models.DateTimeField()
-    e_location = models.CharField(max_length=50)
+    Location = models.CharField(max_length=50)
     """Clarification on registrants required...-Ed"""
     registrants = models.BooleanField()
     """Testing image field height and width. -Ed"""
-    e_image = models.ImageField(height_field=60, width_field=60)
+    e_image = models.ImageField()
     e_adult_price = models.DecimalField(max_digits=5, decimal_places=2)
     e_child_price = models.DecimalField(max_digits=5, decimal_places=2)
 
+def change_button(self, obj):
+    return format_html('<a class="btn" href="/admin/my_app/my_model/{}/change/">Change</a>', obj.id)
+
+def delete_button(self, obj):
+    return format_html('<a class="btn" href="/admin/my_app/my_model/{}/delete/">Delete</a>', obj.id)
+
+list_display = ('__str__', 'change_button', 'delete_button')
